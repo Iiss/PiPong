810b7597d79bd67825f51c64430c50747aa0e9f2
Ilya Alisov <ia13@bk.ru>
2014-05-12 18:12:25 +0400

    Paddle class added

--- pipong.py
+++ pipong.py
@@ -5,18 +5,30 @@ pygame.init()
 
 SCREEN_W = 640
 SCREEN_H = 480
+FOREGROUND = (255,255,255)
+PADDLE_W = 24
+PADDLE_H = 72
 
 SCREEN = pygame.display.set_mode((SCREEN_W,SCREEN_H))
 
 #main loop
 while True:
     for event in pygame.event.get():
-        if event.type==QUIT:
+        if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             sys.exit()
 
     pygame.display.update()
 
+class Paddle(pygame.sprite.Sprite,color,width,height):
 
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+#draw paddle rectangle
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+
+        
     
 
