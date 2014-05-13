0acf62085ea0b37e6647492f84c658a031fa8b8a
Ilya Alisov <ia13@bk.ru>
2014-05-13 12:53:00 +0400

    Main class (App) OOP logic implemented

--- pipong.py
+++ pipong.py
@@ -1,6 +1,18 @@
 import pygame,sys
 from pygame.locals import *
 
+#
+# Global Settings
+#
+SCREEN_W = 640
+SCREEN_H = 480
+FOREGROUND = (255,255,255)
+PADDLE_W = 24
+PADDLE_H = 72
+
+#
+# Paddle
+#
 class Paddle(pygame.sprite.Sprite):
 
     def __init__(self,color,width,height):
@@ -9,37 +21,66 @@ class Paddle(pygame.sprite.Sprite):
         #draw paddle rectangle
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
-
         self.rect=self.image.get_rect()
 
-pygame.init()
+        
+#
+# Main Apllication Class
+#
+class App:
 
-SCREEN_W = 640
-SCREEN_H = 480
-FOREGROUND = (255,255,255)
-PADDLE_W = 24
-PADDLE_H = 72
+    def __init__(self):
+        self._runnting = True
+        self._display_surf = None
+        self.size = SCREEN_W,SCREEN_H
 
-SCREEN = pygame.display.set_mode((SCREEN_W,SCREEN_H))
+    def on_init(self):
+        pygame.init()
+        
+        self._display_surf = pygame.display.set_mode(self.size)
+        pygame.display.set_caption("PiPong")
 
-paddle=Paddle(FOREGROUND,PADDLE_W,PADDLE_H)
+        self._running = True
+        
+#### temp ####
+        paddle=Paddle(FOREGROUND,PADDLE_W,PADDLE_H)
+        self._testGroup = pygame.sprite.Group()
+        self._testGroup.add(paddle)
+#### temp end ####
 
-
-
-
-testGroup = pygame.sprite.Group()
-testGroup.add(paddle)
-testGroup.x=300
-
-#main loop
-while True:
-    for event in pygame.event.get():
+    def on_event(self,event):
         if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            pygame.quit()
-            sys.exit()
+            self._running = False
 
-    testGroup.draw(SCREEN)
-    pygame.display.update()
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        self._testGroup.draw(self._display_surf)
+
+    def on_cleanup(self):
+        pygame.quit()
+        sys.exit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while self._running:
+            for event in pygame.event.get():
+                self.on_event(event)
+
+            self.on_loop()
+            self.on_render()
+
+            pygame.display.update()
+
+        self.on_cleanup()
+
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
+
 
 
 
